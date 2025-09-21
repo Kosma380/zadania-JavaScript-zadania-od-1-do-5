fe30e5840d8808fff21333c7b6853c008523d1f7
Kosma380 <kosmasirocki12@gmail.com>
2025-09-21 18:38:47 +0200

    Add files via upload

---
+++ sirocki_zewnetrzny.js
@@ -0,0 +1,2 @@
+document.write("ostatnia modyfikacja strony".fontcolor("FF0000").strike().fontsize(6)+"<br>"); 
+document.write(document.lastModified.fontcolor("800000").bold().strike().fontsize(7)+"<br>"); 
